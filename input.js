f16f639770efac6f8dc30480063ea342c1513cde
Eli Kaplan <elikaplan@me.com>
2021-03-11 04:02:17 +0000

    made the snake able to move in real time!

--- input.js
+++ input.js
@@ -1,10 +1,9 @@
-const handleUserInput = function(key){
-  if (key === '\u0003') {
-    process.exit();    
-}
-};
 
-const setupInput = function() {
+
+let connection;
+
+const setupInput = function(conn) {
+connection = conn;
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
@@ -12,6 +11,24 @@ stdin.resume();
 stdin.on('data', handleUserInput);
 return stdin;
 }
+
+const handleUserInput = function(key){
+  if (key === '\u0003') {
+    process.exit();    
+  }
+  if (key === '\w'){
+    connection.write('Move: up')
+  }
+  if (key === '\s'){
+    connection.write('Move: down')
+  }
+  if (key === '\a'){
+    connection.write('Move: left')
+  }
+  if (key === '\d'){
+    connection.write('Move: right')
+  }
+};
 
 const obj = {setupInput: setupInput}
 
