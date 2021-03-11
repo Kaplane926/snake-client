0bc75804f0dd01e1be6c6b25326facfed582a3f7
Eli Kaplan <elikaplan@me.com>
2021-03-11 02:06:30 +0000

    added the ability to send commands to the server

--- client.js
+++ client.js
@@ -14,7 +14,7 @@ const connect = function() {
   });
   conn.on('connect', ()=>console.log('Successfully connected to game server'));
   conn.on('connect', ()=> {
-    conn.write('Name: Eli')
+    conn.write('Name: Eli');
   });
 
   return conn;
